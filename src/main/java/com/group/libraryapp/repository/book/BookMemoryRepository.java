d6a6ccd26d3b7d784406a282ae072b55665fbf15
황인상 <star4381@naver.com>
2023-02-25 13:55:43 +0900

    Initial commit

---
+++ src/main/java/com/group/libraryapp/repository/book/BookMemoryRepository.java
@@ -0,0 +1,21 @@
+package com.group.libraryapp.repository.book;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+@Primary
+@Repository
+public class BookMemoryRepository implements BookRepository {
+
+    // private final List<Book> books = new ArrayList<Book>();
+
+    @Override
+    public void saveBook() {
+        System.out.println("Memory");
+    }
+
+}
